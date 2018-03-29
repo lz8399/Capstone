ae7cf36eece45e02d7607674c62954f2f7310d3a
Kevin Dorosh <kcdorosh@gmail.com>
2018-03-29 16:16:08 -0400

    Updated to newest version of WebODM.

--- src/WebODM/app/static/app/js/components/NewTaskPanel.jsx
+++ src/WebODM/app/static/app/js/components/NewTaskPanel.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import EditTaskForm from './EditTaskForm';
 import PropTypes from 'prop-types';
 import Storage from '../classes/Storage';
+import ResizeModes from '../classes/ResizeModes';
 
 class NewTaskPanel extends React.Component {
   static defaultProps = {
@@ -13,6 +14,7 @@ class NewTaskPanel extends React.Component {
 
   static propTypes = {
       onSave: PropTypes.func.isRequired,
+      onCancel: PropTypes.func,
       name: PropTypes.string,
       filesCount: PropTypes.number,
       showResize: PropTypes.bool
@@ -24,14 +26,14 @@ class NewTaskPanel extends React.Component {
     this.state = {
       name: props.name,
       editTaskFormLoaded: false,
-      resize: Storage.getItem('do_resize') !== null ? Storage.getItem('do_resize') == "1" : true,
+      resizeMode: Storage.getItem('resize_mode') === null ? ResizeModes.YES : ResizeModes.fromString(Storage.getItem('resize_mode')),
       resizeSize: parseInt(Storage.getItem('resize_size')) || 2048
     };
 
     this.save = this.save.bind(this);
     this.handleFormTaskLoaded = this.handleFormTaskLoaded.bind(this);
     this.getTaskInfo = this.getTaskInfo.bind(this);
-    this.setResize = this.setResize.bind(this);
+    this.setResizeMode = this.setResizeMode.bind(this);
     this.handleResizeSizeChange = this.handleResizeSizeChange.bind(this);
   }
 
@@ -39,19 +41,28 @@ class NewTaskPanel extends React.Component {
     e.preventDefault();
     this.taskForm.saveLastPresetToStorage();
     Storage.setItem('resize_size', this.state.resizeSize);
-    Storage.setItem('do_resize', this.state.resize ? "1" : "0");
+    Storage.setItem('resize_mode', this.state.resizeMode);
     if (this.props.onSave) this.props.onSave(this.getTaskInfo());
+  }
+
+  cancel = (e) => {
+    if (this.props.onCancel){
+      if (window.confirm("Are you sure you want to cancel?")){
+        this.props.onCancel();
+      }
+    }
   }
 
   getTaskInfo(){
     return Object.assign(this.taskForm.getTaskInfo(), {
-      resizeTo: (this.state.resize && this.state.resizeSize > 0) ? this.state.resizeSize : null
+      resizeSize: this.state.resizeSize,
+      resizeMode: this.state.resizeMode 
     });
   }
 
-  setResize(flag){
+  setResizeMode(v){
     return e => {
-      this.setState({resize: flag});
+      this.setState({resizeMode: v});
     }
   }
 
@@ -76,43 +87,42 @@ class NewTaskPanel extends React.Component {
             ref={(domNode) => { if (domNode) this.taskForm = domNode; }}
           />
 
-          <div className="form-group">
-            <label className="col-sm-2 control-label">Resize Images</label>
-            <div className="col-sm-10">
-              <div className="btn-group">
-                <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                  {this.state.resize ? 
-                   "Yes" : "Skip"} <span className="caret"></span>
-                </button>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a href="javascript:void(0);" 
-                        onClick={this.setResize(true)}>
-                        <i style={{opacity: this.state.resize ? 1 : 0}} className="fa fa-check"></i> Yes</a>
-                  </li>
-                  <li>
-                    <a href="javascript:void(0);" 
-                        onClick={this.setResize(false)}>
-                        <i style={{opacity: !this.state.resize ? 1 : 0}} className="fa fa-check"></i> Skip</a>
-                  </li>
-                </ul>
-              </div>
-              <div className={"resize-control " + (!this.state.resize ? "hide" : "")}>
-                <input 
-                  type="number" 
-                  step="100"
-                  className="form-control"
-                  onChange={this.handleResizeSizeChange} 
-                  value={this.state.resizeSize} 
-                />
-                <span>px</span>
+          {this.state.editTaskFormLoaded ?
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Resize Images</label>
+              <div className="col-sm-10">
+                <div className="btn-group">
+                  <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    {ResizeModes.toHuman(this.state.resizeMode)} <span className="caret"></span>
+                  </button>
+                  <ul className="dropdown-menu">
+                    {ResizeModes.all().map(mode =>
+                      <li key={mode}>
+                        <a href="javascript:void(0);" 
+                            onClick={this.setResizeMode(mode)}>
+                            <i style={{opacity: this.state.resizeMode === mode ? 1 : 0}} className="fa fa-check"></i> {ResizeModes.toHuman(mode)}</a>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+                <div className={"resize-control " + (this.state.resizeMode === ResizeModes.NO ? "hide" : "")}>
+                  <input 
+                    type="number" 
+                    step="100"
+                    className="form-control"
+                    onChange={this.handleResizeSizeChange} 
+                    value={this.state.resizeSize} 
+                  />
+                  <span>px</span>
+                </div>
               </div>
             </div>
-          </div>
+          : ""}
 
           {this.state.editTaskFormLoaded ? 
             <div className="form-group">
               <div className="col-sm-offset-2 col-sm-10 text-right">
+                {this.props.onCancel !== undefined && <button type="submit" className="btn btn-danger" onClick={this.cancel} style={{marginRight: 4}}><i className="glyphicon glyphicon-remove-circle"></i> Cancel</button>}
                 <button type="submit" className="btn btn-primary" onClick={this.save}><i className="glyphicon glyphicon-saved"></i> Start Processing</button>
               </div>
             </div>
